94730c9ee8ec8d85042f499d8b631dd092a768b4
Kelvin-Ziqi-Zhao <Kelvin.Ziqi.Zhao@outlook.com>
2024-03-03 03:29:25 +0800

    Successfully access sd card and write text file

--- Program/ina226_config/spi_access.py
+++ Program/ina226_config/spi_access.py
@@ -1,2 +1,9 @@
 from machine import Pin, SPI
+import machine, sdcard, os
+
 spi = SPI(0, baudrate=10000000, polarity=0, phase=0, sck=Pin(2), mosi=Pin(3), miso=Pin(4))
+sd = sdcard.SDCard(spi, Pin(1))
+
+os.mount(sd, '/sd')
+with open("/sd/test.txt", "w") as f:
+    f.write("Hello world!\r\n")
